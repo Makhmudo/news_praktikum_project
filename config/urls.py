65364118ccac9d5f33b6657cf01fc24c6212b52d
Makhmudo <81033585+Makhmudo@users.noreply.github.com>
2023-06-05 01:49:07 +0900

    latest changes deploymant

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,18 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 from config import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+] + i18n_patterns(
+    path('i18/', include('django.conf.urls.i18n')),
     path('', include('news_app.urls')),
     path('account/', include('accounts.urls')),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
